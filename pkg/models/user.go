cfb5dbe3528393f1483c3fa74c3f4efb533a6cb8
0xMarvell <rokemarvellous@gmail.com>
2022-09-17 14:40:51 +0100

    built middleware for validating JWT token and granting authorization to user

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -2,9 +2,10 @@ package models
 
 import "gorm.io/gorm"
 
+// User is the database model for user objects
 type User struct {
 	gorm.Model
 	Name     string `json:"name"`
 	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"password"`
+	Password string `json:"-"`
 }
--- pkg/routes/userRoutes.go
+++ pkg/routes/userRoutes.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"github.com/0xMarvell/go-jwt-auth-implementation/pkg/controllers"
+	"github.com/0xMarvell/go-jwt-auth-implementation/pkg/middleware"
 	"github.com/gin-gonic/gin"
 )
 
+// RegisterUserRoutes handles all routing for the API
 func RegisterUserRoutes(r *gin.Engine) {
-	// panic("not completed")
 	r.POST("/api/v1/user/signup", controllers.Signup)
 	r.POST("/api/v1/user/login", controllers.Login)
+	r.GET("/api/v1/user/details", middleware.RequireAuth, controllers.GetUserDetails)
 }
